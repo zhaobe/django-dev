5c3702758f4ecde1df89abe0dd3191625dc429f7
zhaobe <zhaoben777@gmail.com>
2017-02-20 22:46:04 -0800

    Added detail page for raising 404 error.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
 #from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -10,7 +10,8 @@ from .models import Question
 #    return HttpResponse("Hello world, welcome to polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
     return HttpResponse(response % question_id)
@@ -20,8 +21,7 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
-        'latest_question_list' : latest_question_list,
+        'latest_question_list' : latest_question_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
