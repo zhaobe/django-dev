f7ed3b130b6a9986c0913c752b981eb1b9b04c8f
zhaobe <zhaoben777@gmail.com>
2017-03-01 00:23:18 -0800

    Adding two more tests for past questions and questions within a day.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -14,3 +14,16 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+    
+    # was_published_recently() should return False for questions whose pub_date is older than 1 day.
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+    
+    # was_published_recently() should return True for questions whose pub_date is within the last day.
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+        
