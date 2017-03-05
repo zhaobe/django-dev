3ddf11b7d50cdd0a82b41a8ca2ea6cf7fb4e11e1
zhaobe <zhaoben777@gmail.com>
2017-03-05 00:47:52 -0800

    Splitted form into fieldsets and added a Choice option for Django admin.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    # for splitting form into fieldsets
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
