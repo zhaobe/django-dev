184552ee0d8de30d50e218a161911d12778b302c
zhaobe <zhaoben777@gmail.com>
2017-03-05 00:52:21 -0800

    Added a class for choices and set it to be tabular inline format (table).

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,12 +4,16 @@ from .models import Choice, Question
 
 # Register your models here.
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     # for splitting form into fieldsets
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
